6fe746cd78ce4b6996edd22bae9dbe8c4ab45efa
Sara Hopper <sara.hopper@gmail.com>
2017-09-20 07:14:32 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,10 +1,10 @@
 class SongsWorker
   require 'csv'
   include Sidekiq::Worker
- 
+
   def perform(songs_file)
-    CSV.foreach(leads_file, headers: true) do |lead|
-      Customer.create(title: lead[0], artist_name: lead[1])
+    CSV.foreach(songs_file, headers: true) do |song|
+      Customer.create(title: song[0], artist_name: song[1])
     end
   end
 end
