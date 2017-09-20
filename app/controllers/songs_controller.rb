4371866bd58fd632f5567946f563268ae69e8898
Sara Hopper <sara.hopper@gmail.com>
2017-09-20 07:22:21 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,11 +23,9 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
-    redirect_to songs_path
-  end
+     SongsWorker.perform_async(params[:file].path)
+     redirect_to songs_path
+   end
 
   def edit
     @song = Song.find(params[:id])
